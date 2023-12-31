d96cb848153f4a9d45a7eb60663beffc91bba416
debarshibasak <debarshi@adaptive.live>
2023-12-31 10:30:05 -0500

    help added

--- cmd/root.go
+++ cmd/root.go
@@ -19,7 +19,7 @@ dbchaos -h for help
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-
+		cmd.Help()
 	},
 }
 
